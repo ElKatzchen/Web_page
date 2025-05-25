a247fb8ee4617d986c85a8a91708d4807be657e3
Katzchen <sesecastro.s@gmail.com>
2025-05-25 03:27:07 -0400

    Max_Pills

--- Pill_Dispencer/api/views.py
+++ Pill_Dispencer/api/views.py
@@ -9,3 +9,10 @@ class UserViewSet(viewsets.ModelViewSet):
 class MedicineScheduleViewSet(viewsets.ModelViewSet):
     queryset = MedicineSchedule.objects.all()
     serializer_class = MedicineScheduleSerializer
+
+    def create(self, request, *args, **kwargs):
+        user = request.user  # o el paciente al que corresponda
+        medicine_count = MedicineSchedule.objects.filter(user=user).count()
+        if medicine_count >= 6:
+            return Response({"Max medicine is 6"}, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
